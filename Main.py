1b01dbd2a2f5ebef2b697f85bd0424c26ee023e5
Robin Dunn <58361313+robin-dunn@users.noreply.github.com>
2020-07-15 17:22:51 +0100

    Added run.sh bash script to run main python script

--- Main.py
+++ Main.py
@@ -1,7 +1,8 @@
 # Language: Python v3
 # Description: Script to read OSM network data and calculate walking routes
+import sys
 from Network import Network
 from ShortestPath import ShortestPath
 
 network = Network.loadData('./graph.dat')
-ShortestPath.dijkstraSearch(network, 314185938, 1320557460)
+ShortestPath.dijkstraSearch(network, int(sys.argv[1]), int(sys.argv[2]))
